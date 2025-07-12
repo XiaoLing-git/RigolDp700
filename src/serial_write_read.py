eae4de418404113c713464bd4dc996a272fcd430
XiaoLing <samuel.ling.xiao@qq.com>
2025-07-12 17:03:34 +0800

    add log info

--- src/serial_write_read.py
+++ src/serial_write_read.py
@@ -33,38 +33,44 @@ class SerialWriteRead:
         return self.__ser
 
     def connect(self) -> None:
-        if self.serial is None:
-            logger.debug(f"{self.__class__.__name__} Serial Port Connecting~~~")
-            self.__ser = SerialConnection(
-                port=self.__port,
-                baud=self.__baud,
-                bytesize=self.__data_bits,
-                parity=self.__parity,
-                stop_bits=self.__stop_bits,
-                timeout=self.__timeout,
-            )
+        try:
+            if self.serial is None:
+                logger.debug(f"{self.__class__.__name__} Serial Port Connecting~~~")
+                self.__ser = SerialConnection(
+                    port=self.__port,
+                    baud=self.__baud,
+                    bytesize=self.__data_bits,
+                    parity=self.__parity,
+                    stop_bits=self.__stop_bits,
+                    timeout=self.__timeout,
+                )
+                logger.debug(
+                    f"{CURRENT_FILE_NAME}.{self.__class__.__name__} Serial Port Connected Already"
+                )
+            elif not self.serial.serial.is_open:
+                logger.debug(
+                    f"{CURRENT_FILE_NAME}.{self.__class__.__name__} Serial Port Closed Already, Try Open It Again"
+                )
+                self.serial.serial.open()
+                logger.debug(
+                    f"{CURRENT_FILE_NAME}.{self.__class__.__name__} Serial Port Connected Already"
+                )
+            else:
+                logger.debug(
+                    f"{CURRENT_FILE_NAME}.{self.__class__.__name__} Serial Port Connected Already"
+                )
+        except Exception as e:
             logger.debug(
-                f"{CURRENT_FILE_NAME}.{self.__class__.__name__} Serial Port Connected Already"
+                f"{CURRENT_FILE_NAME}.{self.__class__.__name__} Connection Error, Msg = {e}"
             )
-        elif not self.serial.serial.is_open:
-            logger.debug(
-                f"{CURRENT_FILE_NAME}.{self.__class__.__name__} Serial Port Closed Already, Try Open It Again"
-            )
-            self.serial.serial.open()
-            logger.debug(
-                f"{CURRENT_FILE_NAME}.{self.__class__.__name__} Serial Port Connected Already"
-            )
-        else:
-            logger.debug(
-                f"{CURRENT_FILE_NAME}.{self.__class__.__name__} Serial Port Connected Already"
-            )
+            raise SerialConnectError(f"Connection Error, Msg = {e}")
 
     def disconnect(self) -> None:
         if self.serial is not None and self.serial.serial.is_open:
             logger.debug(
                 f"{CURRENT_FILE_NAME}.{self.__class__.__name__} Serial Port Disconnecting~~~"
             )
-            self.serial.serial.close()
+            self.serial.disconnect()
         logger.debug(
             f"{CURRENT_FILE_NAME}.{self.__class__.__name__} Serial Port Disconnected Already"
         )
@@ -72,6 +78,9 @@ class SerialWriteRead:
     def read(self) -> str:
         if self.serial is not None and self.serial.serial.is_open:
             return self.serial.read().decode()
+        logger.debug(
+            f"{CURRENT_FILE_NAME}.{self.__class__.__name__} Try Read Data Before Connected"
+        )
         raise SerialConnectError("Try Read Data Before Connected")
 
     def write(self, cmd: str) -> None:
@@ -79,4 +88,7 @@ class SerialWriteRead:
         if self.serial is not None and self.serial.serial.is_open:
             self.serial.write(cmd.encode())
             return
+        logger.debug(
+            f"{CURRENT_FILE_NAME}.{self.__class__.__name__} Try Read Data Before Connected"
+        )
         raise SerialConnectError("Try Write Data Before Connected")
