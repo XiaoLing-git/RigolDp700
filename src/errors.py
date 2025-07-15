11ee51a383220232c81646147b5b059393c5f367
XiaoLing <samuel.ling.xiao@qq.com>
2025-07-15 12:04:45 +0800

    add comments for errors

--- src/errors.py
+++ src/errors.py
@@ -10,36 +10,52 @@ class SerialException(Exception):
 
 
 class SerialConnectError(SerialException):
+    """Raise Error while serial connect fail."""
+
     pass
 
 
 class SerialDisconnectError(SerialException):
+    """Raise Error while serial disconnect fail."""
+
     pass
 
 
 class SerialReadConnectError(SerialException):
+    """Raise Error while serial read before connect."""
+
     pass
 
 
 class SerialWriteConnectError(SerialException):
-    pass
+    """Raise Error while serial write before connect."""
 
 
 class ParseStrToModelException(SerialException):
+    """Raise Error while parse response to model fail."""
+
     pass
 
 
 class ResetException(SerialException):
+    """Raise Error while reset fail."""
+
     pass
 
 
 class ChannelNotExistException(SerialException):
+    """Raise Error while channel not exist."""
+
     pass
 
 
 class ApplySetupError(SerialException):
+    """Raise Error while apply setup value error."""
+
     pass
 
 
 class ClearAlarmException(SerialException):
+    """Raise Error while clear alarm flag fail."""
+
     pass
